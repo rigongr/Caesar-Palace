683e41ba894b70980699e9285a1d0201866b3619
unknown <grapcirigon@gmail.com>
2020-05-24 18:35:41 +0200

    Form validation is completed, contact page is done

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,5 @@
 $(document).ready();
 
-// Form validation
-
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-let message = document.getElementById('message');
-let form = docuemnt.getElementById('form');
-
-function validate() {
-    if(name.value == "") {
-        name.placeholder = "You can't leave your name blank."
-        name.style.borderBottom = "solid 1px red"
-        name.classList.add('color');
-    }
-}
-
-
 $(function () {
     $('.mouse-scroll').click(function (){
     $('html, body').animate({
@@ -24,3 +8,32 @@ $(function () {
 })
   });
 
+
+// Form validation
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+let form = docuemnt.getElementById('form');
+
+function validate() {
+    if(name.value == "" || name.value.length < 2 || name.value == 09) {
+        name.placeholder = "Name invalid."
+        name.style.borderBottom = "solid 1px red"
+        name.classList.add('color');
+        return false;
+    } else {
+        name.style.borderBottom = "solid 1px white";
+    }
+
+    if(email.value.indexOf("@") == -1 || email.value.length < 6 || email.value == "" || email.value.indexOf(".com") == -1) {
+        email.placeholder = "Insert a valid email"
+        email.style.borderBottom = "solid 1px red"
+        name.classList.add('color');
+        return false;
+    } else {
+            email.style.borderBottom = "solid 1px white";
+    };
+
+    return true;
+};
