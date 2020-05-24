fc9d7b70c9ea986e006d627dd5f908b2feaf69a2
unknown <grapcirigon@gmail.com>
2020-05-24 04:18:08 +0200

    I've updated Contact form, made it responsive and also the page now knows when the name section is blank.

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,21 @@
 $(document).ready();
 
+// Form validation
+
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+let form = docuemnt.getElementById('form');
+
+function validate() {
+    if(name.value == "") {
+        name.placeholder = "You can't leave your name blank."
+        name.style.borderBottom = "solid 1px red"
+        name.classList.add('color');
+    }
+}
+
+
 $(function () {
     $('.mouse-scroll').click(function (){
     $('html, body').animate({
@@ -7,3 +23,4 @@ $(function () {
     },1000);  
 })
   });
+
